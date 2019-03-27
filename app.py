a3c61dba14cd0cd123714c1028fdd507076b10f3
sfedetsov <stan.fedetsov@gmail.com>
2019-03-27 22:01:18 +0300

    Small change

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app         = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<h3>Stan Fedetsov. Configuration Project.</h3>"
+    return "<h3>Stan Fedetsov. Configuration Project.</h3><p>This is a test.</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=HTTP_PORT)
